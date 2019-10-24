a01da31a4cfbc005096bbe6af3006c1525f35ca1
Herman1994 <33741898+Herman1994@users.noreply.github.com>
2019-10-24 17:28:47 +0200

    Update time.sh

--- time.sh
+++ time.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Minecraft Time Setter
-# Github: https://github.com/cranstonide/linux-minecraft-scripts
+# Github: https://github.com/Herman1994/Minecraft-Linux-Command-Service/
 
 # Move into the directory with all Linux-Minecraft-Scripts
 cd "$( dirname $0 )"
 
 # Read configuration file
-source mc-config.cfg
+source config.cfg
 
 # Make sure there was an argument passed into the script
 if [ $# -eq 0 ]
 then
     echo "You did not specify a time to set the server to."
-    echo "Usage: ./mc-time.sh TIME"
+    echo "Usage: mcs TIME"
     exit $E_NO_ARGS
 fi
 
@@ -21,7 +21,7 @@ fi
 if ! [[ "$1" =~ ^[0-9]+$ ]]
 then
     echo "Expecting integer, recieved $1."
-    echo "Usage: ./mc-time.sh TIME"
+    echo "Usage: mcs TIME"
     exit 1
 fi
 
@@ -34,7 +34,7 @@ then
 fi
 
 # Send the time set command into
-screen -p 0 -S svr1-main -X eval "stuff \"time set $1\"\015"
+screen -p 0 -S minecraft -X eval "stuff \"time set $1\"\015"
 
 # (Optionally) Tell the players that the time has changed.
 # screen -p 0 -S svr1-main -X eval "stuff \"say Time has been set to $1.\"\015"
