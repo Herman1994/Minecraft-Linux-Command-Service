0e1b5502a55fdf804c15acdd8aec4bd59bc8dabd
Herman1994 <33741898+Herman1994@users.noreply.github.com>
2019-10-24 17:22:28 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Minecraft Startup Script
-# Github: https://github.com/cranstonide/linux-minecraft-scripts
+# Github: https://github.com/Herman1994/Minecraft-Linux-Command-Service/
 
 # Move into the directory with all Linux-Minecraft-Scripts
 cd "$( dirname $0 )"
 
 # Read configuration file
-source mc-config.cfg
+source config.cfg
 
 # Move into the minecraft directory.
 cd $minecraftDir
 
 # Start the game in a GNU screen. It will detatch automatically.
-screen -dmS svr1-main java -Xms16G -Xmx16G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -jar $minecraftJar nogui
+screen -dmS minecraft java -Xms1G -Xmx2G -jar $minecraftJar nogui
