3c8bde8bfe53336d93e58f1d59a814ef5ac005cc
Herman1994 <33741898+Herman1994@users.noreply.github.com>
2019-10-24 17:24:49 +0200

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # Minecraft Stop Script
-# Github: https://github.com/cranstonide/linux-minecraft-scripts
+# Github: https://github.com/Herman1994/Minecraft-Linux-Command-Service/
 
 # Move into the directory with all Linux-Minecraft-Scripts
 cd "$( dirname $0 )"
 
 # Read configuration file
-source mc-config.cfg
+source config.cfg
 
 # Alert the players that the server is going to be shut down. We stuff the `stop` command into
 # the screen session. \015 is the escape value for a return.
-screen -p 0 -S svr1-main -X eval "stuff \"say Server will be shutting down in 10 seconds.\"\015"
+screen -p 0 -S minecraft -X eval "stuff \"say Server will be shutting down in 10 seconds.\"\015"
 
 # Wait a moment and stop the server.
 sleep 10
-screen -p 0 -S svr1-main -X eval "stuff \"stop\"\015"
+screen -p 0 -S minecraft -X eval "stuff \"stop\"\015"
