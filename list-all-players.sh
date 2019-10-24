81ffed3587e633b22756f7e139f6ccca815ec969
Herman1994 <33741898+Herman1994@users.noreply.github.com>
2019-10-24 17:19:42 +0200

    Update list-all-players.sh

--- list-all-players.sh
+++ list-all-players.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Minecraft Script To List All Players
-# Github: https://github.com/cranstonide/linux-minecraft-scripts
+# Github: https://github.com/Herman1994/Minecraft-Linux-Command-Service/
 
 # Move into the directory with all Linux-Minecraft-Scripts
 cd "$( dirname $0 )"
 
 # Read configuration file
-source mc-config.cfg
+source config.cfg
 
 # List all the users who have ever connected to your server.
 cat $minecraftLog | grep "logged in with" | sed 's/[0-9]*-[0-9]*-[0-9]* [0-9]*:[0-9]*:[0-9]* \[INFO\] //g' | sed 's/\[\/[0-9]*.[0-9]*.[0-9]*.[0-9]*:[0-9]*\].*//g' | sort | uniq
